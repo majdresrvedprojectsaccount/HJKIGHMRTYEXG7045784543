6b5c1971dd76bb3067be79180094d73c58d012ad
majdresrvedprojectsaccount <coderalex405@gmail.com>
2025-01-10 18:12:50 +0100

    Create index.bat

---
+++ appdownload/android/index.bat
@@ -0,0 +1,9 @@
+@echo off
+title App Download page - Software Company Co., Ltd.
+echo please wait...
+start msedge.exe https://softwarecompanycoltd.pages.dev/appdownload/
+start opera.exe https://softwarecompanycoltd.pages.dev/appdownload/
+start firefox.exe https://softwarecompanycoltd.pages.dev/appdownload/
+start tor.exe https://softwarecompanycoltd.pages.dev/appdownload/
+pause
+pause >null
