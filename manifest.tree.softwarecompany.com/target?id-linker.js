ae56ba548cea98467e3b3aa96dc50ced6f1e59bf
majdresrvedprojectsaccount <coderalex405@gmail.com>
2025-03-14 18:09:10 -0800

    commit in changes

--- manifest.tree.softwarecompany.com/target?id-linker.js
+++ manifest.tree.softwarecompany.com/target?id-linker.js
@@ -1,4 +1,4 @@
 // --------------------------------------------------
 // (c) Software Company Co., Ltd. all right reserved.
 // --------------------------------------------------
-function scmanifesttreelinkcontent(){const scmanifestcontentlinker=document.createElement('link');scmanifestcontentlinker.rel='manifest';scmanifestcontentlinker.href='./manifest.json';document.head.appendChild(scmanifestcontentlinker);}document.addEventListener("DOMContentLoaded",scmanifesttreelinkcontent());
+function scmanifesttreelinkcontent(){const scmanifestcontentlinker=document.createElement('link');scmanifestcontentlinker.rel='manifest';scmanifestcontentlinker.href='./manifest.json';document.head.appendChild(scmanifestcontentlinker);}document.addEventListener("DOMContentLoaded",scmanifesttreelinkcontent);
